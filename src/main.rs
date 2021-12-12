d75cc4f6dc6a172010c1e46ff6fc25929922fcff
zephyrj <zephyrj@protonmail.com>
2021-12-12 20:06:01 +0000

    - Put steam related functions into their own module - Add function to get BeamNG mod path

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,34 @@ along with sim-racing-tools. If not, see <https://www.gnu.org/licenses/>.
 */
 
 fn main() {
-    println!("The game is referred to as {} and its game id is {}",
-             assetto_corsa::steam::GAME_NAME,
-             assetto_corsa::steam::GAME_ID);
-
     if assetto_corsa::is_installed() {
-        println!("It is installed");
+        println!("Assetto Corsa is installed");
         println!("Installed cars can be found at {}",
                  assetto_corsa::get_installed_cars_path().unwrap().display())
     } else {
-        println!("It is not installed");
+        println!("Assetto Corsa is not installed");
         return;
     }
+
+    if automation::is_installed() {
+        println!("Automation is installed");
+    } else {
+        println!("Automation is not installed");
+        return;
+    }
+
+    println!("BeamNG mod folder resolved to {}", beam_ng::get_mod_path().unwrap().display());
 }
 
 mod assetto_corsa {
     use std::path::PathBuf;
+    use crate::steam;
+
+    pub const STEAM_GAME_NAME: &str = "assettocorsa";
+    pub const STEAM_GAME_ID: i64 = 244210;
 
     pub fn is_installed() -> bool {
-        if let Some(install_path) = steam::get_install_path() {
+        if let Some(install_path) = steam::get_install_path(STEAM_GAME_NAME) {
             install_path.is_dir()
         } else {
             false
@@ -46,7 +55,7 @@ mod assetto_corsa {
     }
 
     pub fn get_installed_cars_path() -> Option<PathBuf> {
-        if let Some(mut install_path) = steam::get_install_path() {
+        if let Some(mut install_path) = steam::get_install_path(STEAM_GAME_NAME) {
             for path in ["content", "cars"] {
                 install_path.push(path)
             }
@@ -55,35 +64,82 @@ mod assetto_corsa {
             None
         }
     }
+}
 
-    pub mod steam {
-        use std::path::PathBuf;
-        use directories::UserDirs;
+mod automation {
+    use crate::steam;
 
-        pub const GAME_NAME: &str = "assettocorsa";
-        pub const GAME_ID: i64 = 244210;
+    pub const STEAM_GAME_NAME: &str = "Automation";
+    pub const STEAM_GAME_ID: i64 = 293760;
 
-        #[cfg(target_os = "linux")]
-        pub fn get_install_path() -> Option<PathBuf> {
-            if let Some(user_dirs) = UserDirs::new() {
-                let mut install_path = PathBuf::from(user_dirs.home_dir());
-                for path in [".steam", "debian-installation", "steamapps", "common", GAME_NAME] {
-                    install_path.push(path);
-                }
-                Some(install_path)
-            } else {
-                None
-            }
-        }
-
-        #[cfg(target_os = "windows")]
-        pub fn get_install_path() -> Option<PathBuf> {
-            Some(PathBuf::from(format!("C:\\Program Files (x86)\\Steam\\steamapps\\common\\{}",
-                                          GAME_NAME)))
+    pub fn is_installed() -> bool {
+        if let Some(install_path) = steam::get_install_path(STEAM_GAME_NAME) {
+            install_path.is_dir()
+        } else {
+            false
         }
     }
 }
 
-mod automation {
+mod beam_ng {
+    use std::path::PathBuf;
+    use directories::BaseDirs;
+    use parselnk::Lnk;
+    use crate::steam;
 
+    pub const STEAM_GAME_NAME: &str = "BeamNG.drive";
+    pub const STEAM_GAME_ID: i64 = 284160;
+
+    pub fn get_mod_path() -> Option<PathBuf> {
+        let dirs = BaseDirs::new().unwrap();
+        let mut mod_path_buf: PathBuf = dirs.cache_dir().to_path_buf();
+        mod_path_buf.push(STEAM_GAME_NAME);
+        match steam::get_install_path(STEAM_GAME_NAME) {
+            Some(_) => {
+                let mut link_path = mod_path_buf.clone();
+                link_path.push("latest.lnk");
+                match Lnk::try_from(link_path.as_path()) {
+                    Ok(lnk) => {
+                        if let Some(target_path) = lnk.link_info.local_base_path {
+                            mod_path_buf = PathBuf::from(target_path);
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+            None => {}
+        }
+        mod_path_buf.push("mods");
+        Some(mod_path_buf)
+    }
+}
+
+pub mod steam {
+    use std::path::PathBuf;
+    #[cfg(target_os = "linux")]
+    use directories::UserDirs;
+
+    #[cfg(target_os = "linux")]
+    pub fn get_install_path(&str: game_name) -> Option<PathBuf> {
+        if let Some(user_dirs) = UserDirs::new() {
+            let mut install_path = PathBuf::from(user_dirs.home_dir());
+            for path in [".steam", "debian-installation", "steamapps", "common", game_name] {
+                install_path.push(path);
+            }
+            Some(install_path)
+        } else {
+            None
+        }
+    }
+
+    #[cfg(target_os = "windows")]
+    pub fn get_install_path(game_name: &str) -> Option<PathBuf> {
+        let path = PathBuf::from(format!("C:\\Program Files (x86)\\Steam\\steamapps\\common\\{}",
+                                     game_name));
+        if path.is_dir() {
+            Some(path)
+        } else {
+            None
+        }
+    }
 }
