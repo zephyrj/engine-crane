eb2f9fc6b8fa2797bf75a5162cd70f2eab9be704
zephyrj <zephyrj@protonmail.com>
2022-03-01 20:54:39 +0000

    WIP: Create function to transform beam_ng mod/automation engine data into an AC engine

---
+++ src/fabricator.rs
@@ -0,0 +1,24 @@
+use std::path::Path;
+use crate::{automation, beam_ng};
+
+pub fn build_ac_engine_from_beam_ng_mod(beam_ng_mod_path: &Path) -> Result<(), String>{
+    let mod_data = beam_ng::extract_mod_data(beam_ng_mod_path).unwrap();
+    let car_file = automation::car::CarFile::from_bytes(mod_data.car_file_data).unwrap();
+    let uid = car_file.get_section("Car").unwrap().get_section("Variant").unwrap().get_attribute("UID").unwrap().value.as_str().unwrap();
+    println!("Car UID = {}", uid);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use crate::beam_ng::get_mod_list;
+    use crate::fabricator::build_ac_engine_from_beam_ng_mod;
+
+    #[test]
+    fn load_mods() -> Result<(), String> {
+        let mods = get_mod_list().unwrap();
+        build_ac_engine_from_beam_ng_mod(PathBuf::from(&mods[0]).as_path());
+        Ok(())
+    }
+}
