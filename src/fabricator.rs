9dca987b846c1c7443dbe5db912eacca07bda375
zephyrj <zephyrj@protonmail.com>
2022-03-03 20:12:38 +0000

    Lookup automation engine data by uuid

--- src/fabricator.rs
+++ src/fabricator.rs
@@ -1,11 +1,17 @@
 use std::path::Path;
 use crate::{automation, beam_ng};
+use crate::automation::sandbox::{EngineV1, load_engine_by_uuid};
 
 pub fn build_ac_engine_from_beam_ng_mod(beam_ng_mod_path: &Path) -> Result<(), String>{
     let mod_data = beam_ng::extract_mod_data(beam_ng_mod_path).unwrap();
     let car_file = automation::car::CarFile::from_bytes(mod_data.car_file_data).unwrap();
     let uid = car_file.get_section("Car").unwrap().get_section("Variant").unwrap().get_attribute("UID").unwrap().value.as_str().unwrap();
     println!("Car UID = {}", uid);
+    let eng = match load_engine_by_uuid(uid)? {
+        None => { return Err(String::from("No engine found")); }
+        Some(eng) => { eng }
+    };
+    println!("{:?}", eng);
     Ok(())
 }
 
