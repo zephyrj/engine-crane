7f86607f2eadaf1e5aeda43875362c7ec6d1d523
zephyrj <zephyrj@protonmail.com>
2022-03-17 21:06:07 +0000

    Wire up the ui to the prototype engine swap implementation

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,7 +1,9 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
-use std::path::Path;
-use iced::{Column, Element, Length, pick_list, PickList, Sandbox, Align, Text, Settings, Error};
-use crate::assetto_corsa;
+use std::ops::Add;
+use std::path::{Path, PathBuf};
+use iced::{Column, Element, Length, pick_list, PickList, Sandbox, Align, Text, Settings, Error, text_input, TextInput, Row, button, Button, Color, HorizontalAlignment};
+use crate::{assetto_corsa, beam_ng, fabricator};
 use crate::automation;
 
 pub fn launch() -> Result<(), Error> {
@@ -11,12 +13,15 @@ pub fn launch() -> Result<(), Error> {
 #[derive(Default)]
 pub struct CarSelector {
     available_cars: Vec<String>,
-    available_engines: HashMap<String, automation::sandbox::EngineV1>,
-    engine_ref: Vec<EngineRef>,
+    available_mods: Vec<String>,
     current_car: Option<String>,
-    current_engine: Option<EngineRef>,
+    current_mod: Option<String>,
+    current_new_spec_name: String,
     car_pick_list: pick_list::State<String>,
-    engine_pick_list: pick_list::State<EngineRef>
+    new_spec_name: text_input::State,
+    mod_pick_list: pick_list::State<String>,
+    swap_button: button::State,
+    status_message: String
 }
 
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
@@ -34,24 +39,25 @@ impl std::fmt::Display for EngineRef {
 #[derive(Debug, Clone)]
 pub enum Message {
     CarSelected(String),
-    EngineSelected(EngineRef)
+    NameEntered(String),
+    ModSelected(String),
+    SwapButtonPressed
+}
+
+fn to_filename_vec(path_vec: &Vec<PathBuf>) -> Vec<String> {
+    path_vec.iter().map(|path|{
+        String::from(path.file_name().unwrap().to_string_lossy())
+    }).collect()
 }
 
 impl Sandbox for CarSelector {
     type Message = Message;
 
     fn new() -> Self {
-        let engines = automation::sandbox::load_engines();
-        let engine_refs: Vec<EngineRef> = engines.iter().map(|(uid, eng)| {
-            EngineRef { uid: String::from(uid), display_name: eng.friendly_name() }
-        }).collect();
+        let mods = to_filename_vec(&beam_ng::get_mod_list());
         CarSelector {
-            available_cars: assetto_corsa::get_list_of_installed_cars().unwrap()
-            .iter()
-            .map(|car_path| String::from(Path::new(car_path.as_os_str()).file_name().unwrap().to_str().unwrap()))
-            .collect(),
-            available_engines: engines,
-            engine_ref: engine_refs,
+            available_cars: to_filename_vec(&assetto_corsa::get_list_of_installed_cars().unwrap()),
+            available_mods: mods,
             ..Default::default() }
     }
 
@@ -62,15 +68,54 @@ impl Sandbox for CarSelector {
     fn update(&mut self, message: Self::Message) {
         match message {
             Message::CarSelected(car_path) => {
-                self.current_car = Some(car_path)
-            }
-            Message::EngineSelected(engine_ref) => {
-                self.current_engine = Some(engine_ref)
+                self.current_car = Some(car_path);
+            },
+            Message::ModSelected(mod_name) => {
+                self.current_new_spec_name = String::from(mod_name.strip_suffix(".zip").unwrap());
+                self.current_mod = Some(mod_name)
+            },
+            Message::NameEntered(new_car_name) => {
+                self.current_new_spec_name = new_car_name
+            },
+            Message::SwapButtonPressed => {
+                if self.current_car.is_none() {
+                    self.status_message = String::from("Please select an Assetto Corsa car");
+                    return;
+                } else if self.current_mod.is_none() {
+                    self.status_message = String::from("Please select an BeamNG mod");
+                    return;
+                }
+                let new_car_path = assetto_corsa::car::create_new_car_spec((&self.current_car).as_ref().unwrap().as_str(),
+                                                                           self.current_new_spec_name.as_str()).unwrap();
+                let mut mod_path = beam_ng::get_mod_path().unwrap();
+                if let Some(mod_name) = &self.current_mod {
+                    mod_path = mod_path.join(Path::new(mod_name.as_str()));
+                }
+                match fabricator::swap_automation_engine_into_ac_car(mod_path.as_path(), new_car_path.as_path()) {
+                    Ok(_) => { self.status_message = format!("Created {} successfully", new_car_path.display()) }
+                    Err(err_str) => { self.status_message = err_str }
+                }
             }
         }
     }
 
     fn view(&mut self) -> Element<Message> {
+        let placeholder = match self.current_new_spec_name.as_str() {
+            "" => { "Enter new car name" }
+            s => { s }
+        };
+        let input = TextInput::new(
+            &mut self.new_spec_name,
+            placeholder,
+            &self.current_new_spec_name,
+            Message::NameEntered,
+        );
+        let car_name_container = Column::new()
+            .align_items(Align::Center)
+            .padding(10)
+            .push(Text::new("New spec name (this will be appended to the created car)"))
+            .push(input);
+
         let car_select_container = Column::new()
             .align_items(Align::Center)
             .padding(10)
@@ -81,22 +126,48 @@ impl Sandbox for CarSelector {
                 self.current_car.clone(),
                 Message::CarSelected,
             ));
-
-        let engine_select_container = Column::new()
+        let mod_select_container = Column::new()
             .align_items(Align::Center)
-            .push(Text::new("Automation engine"))
+            .push(Text::new("BeamNG mod"))
             .push(PickList::new(
-                &mut self.engine_pick_list,
-                &self.engine_ref,
-                self.current_engine.clone(),
-                Message::EngineSelected
+                &mut self.mod_pick_list,
+                &self.available_mods,
+                self.current_mod.clone(),
+                Message::ModSelected
             ));
-
-        Column::new().width(Length::Fill)
+        let select_container = Column::new()
             .align_items(Align::Center)
             .spacing(10)
             .push(car_select_container)
-            .push(engine_select_container)
-            .into()
+            .push(mod_select_container);
+
+        let selection_row = Row::new()
+            .align_items(Align::Center)
+            .push(select_container)
+            .push(car_name_container);
+
+        let swap_button = Button::new(&mut self.swap_button, Text::new("Swap"))
+            .min_width(60)
+            .on_press(Message::SwapButtonPressed);
+        let control_row = Row::new()
+            .align_items(Align::Start)
+            .padding(20)
+            .push(swap_button);
+
+        let mut layout = Column::new().width(Length::Fill)
+            .align_items(Align::Start)
+            .padding(10)
+            .spacing(30)
+            .push(selection_row)
+            .push(control_row);
+
+        if !self.status_message.is_empty() {
+            layout = layout.push(
+                Row::new()
+                    .align_items(Align::Center)
+                    .push(Text::new(self.status_message.as_str()).horizontal_alignment(HorizontalAlignment::Center))
+            )
+        }
+        layout.into()
     }
 }
