d60e1d0d83d38a6f34bab5f431302c73947523e7
zephyrj <zephyrj@protonmail.com>
2022-04-19 10:40:51 +0100

    Split engine data struct into its own file

---
+++ src/assetto_corsa/engine/engine_data.rs
@@ -0,0 +1,42 @@
+use crate::assetto_corsa::traits::{CarDataFile, MandatoryDataSection};
+use crate::assetto_corsa::error::Result;
+use crate::assetto_corsa::ini_utils;
+use crate::assetto_corsa::ini_utils::{Ini, IniUpdater};
+
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct EngineData {
+    pub altitude_sensitivity: f64,
+    pub inertia: f64,
+    pub limiter: i32,
+    pub limiter_hz: i32,
+    pub minimum: i32
+}
+
+impl EngineData {
+    const SECTION_NAME: &'static str = "ENGINE_DATA";
+}
+
+impl MandatoryDataSection for EngineData {
+    fn load_from_parent(parent_data: &dyn CarDataFile) -> Result<Self> where Self: Sized {
+        let ini_data = parent_data.ini_data();
+        Ok(EngineData{
+            altitude_sensitivity: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "ALTITUDE_SENSITIVITY")?,
+            inertia: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "INERTIA")?,
+            limiter: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "LIMITER")?,
+            limiter_hz: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "LIMITER_HZ")?,
+            minimum: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "MINIMUM")?
+        })
+    }
+}
+
+impl IniUpdater for EngineData {
+    fn update_ini(&self, ini_data: &mut Ini) -> std::result::Result<(), String> {
+        ini_utils::set_float(ini_data, Self::SECTION_NAME, "ALTITUDE_SENSITIVITY", self.altitude_sensitivity, 2);
+        ini_utils::set_float(ini_data, Self::SECTION_NAME, "INERTIA", self.inertia, 3);
+        ini_utils::set_value(ini_data, Self::SECTION_NAME, "LIMITER", self.limiter);
+        ini_utils::set_value(ini_data, Self::SECTION_NAME, "LIMITER_HZ", self.limiter_hz);
+        ini_utils::set_value(ini_data, Self::SECTION_NAME, "MINIMUM", self.minimum);
+        Ok(())
+    }
+}
--- src/assetto_corsa/engine/mod.rs
+++ src/assetto_corsa/engine/mod.rs
@@ -1,14 +1,13 @@
 pub mod metadata;
 pub mod fuel_consumption;
+pub mod engine_data;
 
 use std::collections::HashMap;
 use std::io;
 use std::fmt::{Display, Formatter};
-
 use std::path::Path;
 use std::str::FromStr;
 use csv::Terminator;
-
 use crate::assetto_corsa::car::Car;
 use crate::assetto_corsa::error::{Result, Error, ErrorKind, PropertyParseError};
 use crate::assetto_corsa::lut_utils;
@@ -19,45 +18,9 @@ use crate::assetto_corsa::traits::{MandatoryDataSection, CarDataFile, OptionalDa
 use crate::assetto_corsa::structs::LutProperty;
 
 pub use metadata::Metadata;
+pub use engine_data::EngineData;
 pub use fuel_consumption::FuelConsumptionFlowRate;
 
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct EngineData {
-    pub altitude_sensitivity: f64,
-    pub inertia: f64,
-    pub limiter: i32,
-    pub limiter_hz: i32,
-    pub minimum: i32
-}
-
-impl EngineData {
-    const SECTION_NAME: &'static str = "ENGINE_DATA";
-}
-
-impl MandatoryDataSection for EngineData {
-    fn load_from_parent(parent_data: &dyn CarDataFile) -> Result<Self> where Self: Sized {
-        let ini_data = parent_data.ini_data();
-        Ok(EngineData{
-            altitude_sensitivity: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "ALTITUDE_SENSITIVITY")?,
-            inertia: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "INERTIA")?,
-            limiter: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "LIMITER")?,
-            limiter_hz: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "LIMITER_HZ")?,
-            minimum: ini_utils::get_mandatory_property(ini_data, Self::SECTION_NAME, "MINIMUM")?
-        })
-    }
-}
-
-impl IniUpdater for EngineData {
-    fn update_ini(&self, ini_data: &mut Ini) -> std::result::Result<(), String> {
-        ini_utils::set_float(ini_data, Self::SECTION_NAME, "ALTITUDE_SENSITIVITY", self.altitude_sensitivity, 2);
-        ini_utils::set_float(ini_data, Self::SECTION_NAME, "INERTIA", self.inertia, 3);
-        ini_utils::set_value(ini_data, Self::SECTION_NAME, "LIMITER", self.limiter);
-        ini_utils::set_value(ini_data, Self::SECTION_NAME, "LIMITER_HZ", self.limiter_hz);
-        ini_utils::set_value(ini_data, Self::SECTION_NAME, "MINIMUM", self.minimum);
-        Ok(())
-    }
-}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Damage {
