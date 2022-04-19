c6b93d0fc8439d048f06ccb81820d6a84cdc192f
zephyrj <zephyrj@protonmail.com>
2022-04-19 11:20:37 +0100

    Split drivetrain auto-blip struct out into its own file

---
+++ src/assetto_corsa/drivetrain/auto_blip.rs
@@ -0,0 +1,48 @@
+use crate::assetto_corsa::drivetrain::{get_mandatory_field, mandatory_field_error};
+use crate::assetto_corsa::ini_utils;
+use crate::assetto_corsa::ini_utils::{Ini, IniUpdater};
+use crate::assetto_corsa::traits::{CarDataFile, MandatoryDataSection};
+use crate::assetto_corsa::error::Result;
+
+
+#[derive(Debug)]
+pub struct AutoBlip {
+    pub electronic: i32,
+    pub points: Vec<i32>,
+    pub level: f64
+}
+
+impl AutoBlip {
+    fn get_point_key<T: std::fmt::Display>(idx: T) -> String {
+        format!("POINT_{}", idx)
+    }
+}
+
+impl MandatoryDataSection for AutoBlip {
+    fn load_from_parent(parent_data: &dyn CarDataFile) -> Result<Self> where Self: Sized {
+        let ini_data = parent_data.ini_data();
+        let electronic = get_mandatory_field(ini_data, "AUTOBLIP", "ELECTRONIC")?;
+        let level = get_mandatory_field(ini_data, "AUTOBLIP", "LEVEL")?;
+        let mut points = Vec::new();
+        for idx in 0..3 {
+            points.push(get_mandatory_field(ini_data,
+                                            "AUTOBLIP",
+                                            AutoBlip::get_point_key(idx).as_str())?);
+        }
+        Ok(AutoBlip{ electronic, points, level })
+    }
+}
+
+impl IniUpdater for AutoBlip {
+    fn update_ini(&self, ini_data: &mut Ini) -> std::result::Result<(), String> {
+        ini_utils::set_value(ini_data, "AUTOBLIP", "ELECTRONIC", self.electronic);
+        ini_utils::set_float(ini_data, "AUTOBLIP", "LEVEL", self.level, 2);
+        for (idx, point) in self.points.iter().enumerate() {
+            ini_utils::set_value(ini_data,
+                                 "AUTOBLIP",
+                                 AutoBlip::get_point_key(idx).as_str(),
+                                 point);
+        }
+        Ok(())
+    }
+}
--- src/assetto_corsa/drivetrain/mod.rs
+++ src/assetto_corsa/drivetrain/mod.rs
@@ -3,12 +3,14 @@ pub mod gearbox;
 pub mod clutch;
 pub mod differential;
 pub mod auto_clutch;
+pub mod auto_blip;
 
 pub use traction::Traction;
 pub use gearbox::Gearbox;
 pub use clutch::Clutch;
 pub use differential::Differential;
 pub use auto_clutch::AutoClutch;
+pub use auto_blip::AutoBlip;
 
 use crate::assetto_corsa::error::{Result, Error, ErrorKind};
 use crate::assetto_corsa::{ini_utils};
@@ -16,48 +18,6 @@ use crate::assetto_corsa::car::Car;
 use crate::assetto_corsa::ini_utils::{Ini, IniUpdater};
 use crate::assetto_corsa::traits::{CarDataFile, MandatoryDataSection, DataInterface};
 
-
-#[derive(Debug)]
-pub struct AutoBlip {
-    pub electronic: i32,
-    pub points: Vec<i32>,
-    pub level: f64
-}
-
-impl AutoBlip {
-    fn get_point_key<T: std::fmt::Display>(idx: T) -> String {
-        format!("POINT_{}", idx)
-    }
-}
-
-impl MandatoryDataSection for AutoBlip {
-    fn load_from_parent(parent_data: &dyn CarDataFile) -> Result<Self> where Self: Sized {
-        let ini_data = parent_data.ini_data();
-        let electronic = get_mandatory_field(ini_data, "AUTOBLIP", "ELECTRONIC")?;
-        let level = get_mandatory_field(ini_data, "AUTOBLIP", "LEVEL")?;
-        let mut points = Vec::new();
-        for idx in 0..3 {
-            points.push(get_mandatory_field(ini_data,
-                                            "AUTOBLIP",
-                                            AutoBlip::get_point_key(idx).as_str())?);
-        }
-        Ok(AutoBlip{ electronic, points, level })
-    }
-}
-
-impl IniUpdater for AutoBlip {
-    fn update_ini(&self, ini_data: &mut Ini) -> std::result::Result<(), String> {
-        ini_utils::set_value(ini_data, "AUTOBLIP", "ELECTRONIC", self.electronic);
-        ini_utils::set_float(ini_data, "AUTOBLIP", "LEVEL", self.level, 2);
-        for (idx, point) in self.points.iter().enumerate() {
-            ini_utils::set_value(ini_data,
-                                 "AUTOBLIP",
-                                 AutoBlip::get_point_key(idx).as_str(),
-                                 point);
-        }
-        Ok(())
-    }
-}
 
 #[derive(Debug)]
 pub struct AutoShifter {
