6b7510f19a0a775f58a9695fc1d18f338ca349a3
zephyrj <zephyrj@protonmail.com>
2022-09-14 16:43:31 +0100

    Add GPLv3 license headers

--- src/assetto_corsa/car/data/mod.rs
+++ src/assetto_corsa/car/data/mod.rs
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c):
+ * 2022 zephyrj
+ * zephyrj@protonmail.com
+ *
+ * This file is part of engine-crane.
+ *
+ * engine-crane is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * engine-crane is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with engine-crane. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pub mod car_ini_data;
 pub mod drivetrain;
 pub mod engine;
