04565ca58c72ff3b046b89dbbe13b67522617f5c
zephyrj <zephyrj@protonmail.com>
2022-09-17 09:30:12 +0100

    Write a manifest file that requests admin privileges so that the program can write to ProgramFiles

---
+++ build.rs
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c):
+ * 2022 zephyrj
+ * zephyrj@protonmail.com
+ *
+ * This file is part of engine-crane.
+ *
+ * engine-crane is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * engine-crane is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with engine-crane. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    if Ok("release".to_owned()) == std::env::var("PROFILE") {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#);
+        res.compile().unwrap();
+    }
+}
+
+#[cfg(unix)]
+fn main() {
+}
