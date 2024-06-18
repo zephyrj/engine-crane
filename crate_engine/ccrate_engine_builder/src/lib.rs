8625103656a6f585c5bc7f69ae9615d71774a9e9
zephyrj <zephyrj@protonmail.com>
2024-06-18 08:20:54 +0100

    Store an error string in the ccrate_engine_builder lib so it can be queried on error

--- crate_engine/ccrate_engine_builder/src/lib.rs
+++ crate_engine/ccrate_engine_builder/src/lib.rs
@@ -20,7 +20,8 @@
  */
 extern crate core;
 
-use std::ffi::{c_float, c_ulong, CStr};
+use std::cell::RefCell;
+use std::ffi::{c_float, c_ulong, CStr, CString};
 use std::fs;
 use std::os::raw::c_char;
 use std::path::{PathBuf};
@@ -53,6 +54,33 @@ fn attach() {
 fn detach() {
 }
 
+thread_local! {
+    static LAST_ERROR: RefCell<Option<CString>> = RefCell::new(None);
+}
+
+fn set_last_error(err: &str) {
+    let c_string = CString::new(err).unwrap();
+    LAST_ERROR.with(|last_error| {
+        *last_error.borrow_mut() = Some(c_string);
+    });
+}
+
+fn clear_last_error() {
+    LAST_ERROR.with(|last_error| {
+        *last_error.borrow_mut() = None;
+    })
+}
+
+#[no_mangle]
+pub extern fn get_last_error() -> *const c_char {
+    LAST_ERROR.with(|last_error| {
+        match &*last_error.borrow() {
+            Some(err) => err.as_ptr(),
+            None => std::ptr::null(),
+        }
+    })
+}
+
 #[no_mangle]
 pub extern fn init(script_version: u32) -> *mut DataV1 {
     let mut data: Box<DataV1> = Box::new(DataV1::new());
@@ -65,7 +93,7 @@ pub extern fn add_string(instance: *mut DataV1,
                          group: *const c_char,
                          key: *const c_char,
                          val: *const c_char) {
-    let mut data = unsafe { &mut*(instance) };
+    let data = unsafe { &mut*(instance) };
     let group_cstr = unsafe { CStr::from_ptr(group) };
     let key_cstr = unsafe { CStr::from_ptr(key) };
     let val_cstr = unsafe { CStr::from_ptr(val) };
@@ -79,7 +107,7 @@ pub extern fn add_float(instance: *mut DataV1,
                         group: *const c_char,
                         key: *const c_char,
                         val: c_float) {
-    let mut data = unsafe { &mut*(instance) };
+    let data = unsafe { &mut*(instance) };
     let group_cstr = unsafe { CStr::from_ptr(group) };
     let key_cstr = unsafe { CStr::from_ptr(key) };
     data.add_float(String::from_utf8_lossy(group_cstr.to_bytes()).to_string(),
@@ -92,7 +120,7 @@ pub extern fn add_curve_data(instance: *mut DataV1,
                              curve_name: *const c_char,
                              index: c_ulong,
                              val: c_float) {
-    let mut data = unsafe { &mut*(instance) };
+    let data = unsafe { &mut*(instance) };
     let curve_name_cstr = unsafe { CStr::from_ptr(curve_name) };
     data.add_curve_data(String::from_utf8_lossy(curve_name_cstr.to_bytes()).to_string(),
                         index as usize,
@@ -103,6 +131,7 @@ pub extern fn add_curve_data(instance: *mut DataV1,
 pub extern fn dump_json(instance: *mut DataV1,
                         path_char: *const c_char) -> bool
 {
+    clear_last_error();
     let data = unsafe { &mut*(instance) };
     let path_cstr = unsafe { CStr::from_ptr(path_char) };
     let path_str = String::from_utf8_lossy(path_cstr.to_bytes()).to_string();
@@ -115,14 +144,17 @@ pub extern fn dump_json(instance: *mut DataV1,
     }
     match fs::create_dir_all(&parent_path) {
         Ok(_) => {}
-        Err(_) => {
+        Err(e) => {
+            set_last_error(&format!("Failed to create directory ({}) to write json dump to. {}",
+                                    parent_path.display(), e.to_string()));
             return false;
         }
     }
     let json_string = match serde_json::to_string_pretty(&data) {
         Ok(s) => s,
         Err(e) => {
-            println!("{}", e);
+            set_last_error(&format!("Failed to encode data as json. {}",
+                                    e.to_string()));
             return false;
         }
     };
@@ -132,7 +164,11 @@ pub extern fn dump_json(instance: *mut DataV1,
                                                         "json");
     match fs::write(file_path, json_string) {
         Ok(_) => true,
-        Err(_) => false
+        Err(e) => {
+            set_last_error(&format!("Failed to write json file. {}",
+                                    e.to_string()));
+            false
+        }
     }
 }
 
@@ -140,6 +176,7 @@ pub extern fn dump_json(instance: *mut DataV1,
 pub extern fn finalise(instance: *mut DataV1,
                        path_char: *const c_char) -> bool
 {
+    clear_last_error();
     let data = unsafe {Box::from_raw(instance)};
     let path_cstr = unsafe { CStr::from_ptr(path_char) };
     let path_str = String::from_utf8_lossy(path_cstr.to_bytes()).to_string();
@@ -157,6 +194,8 @@ pub extern fn finalise(instance: *mut DataV1,
             match fs::create_dir_all(&path) {
                 Ok(_) => {}
                 Err(e) => {
+                    set_last_error(&format!("Failed to create directory ({} )to write crate engine to. {}",
+                                            path.display(), e.to_string()));
                     return false;
                 }
             }
@@ -165,6 +204,8 @@ pub extern fn finalise(instance: *mut DataV1,
                     true
                 }
                 Err(e) => {
+                    set_last_error(&format!("Failed to write crate engine file. {}",
+                                            e.to_string()));
                     false
                 }
             }
