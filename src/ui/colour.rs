3f26060c95b62cf556d40894a17e54a9f7557ff6
zephyrj <zephyrj@protonmail.com>
2024-10-05 14:50:48 +0100

    Add file for colours select funcs

---
+++ src/ui/colour.rs
@@ -0,0 +1,13 @@
+use iced::Color;
+
+pub fn success_green() -> Color {
+    Color::from_rgb8(75, 181, 67)
+}
+
+pub fn warn_yellow() -> Color {
+    Color::from_rgb8(233, 213, 2)
+}
+
+pub fn fail_red() -> Color {
+    Color::from_rgb8(237, 67, 55)
+}
